73f02a7cffd7fcccab2329e525567a0fccc5307b
Justin Buchanan <justinbuchanan@google.com>
2019-07-01 14:38:52 -0700

    touch file

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
   Eigen::Vector2f v{1, 2};
-  cout << "point: {" << v << "}" << endl;
+  cout << "Point: {" << v << "}" << endl;
 
   return 0;
 }
